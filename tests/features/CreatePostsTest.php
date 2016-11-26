0a3fbea55ab787b702db5dc765dbec821e898bd1
Daniel Lopez <tierra_santa_9@hotmail.com>
2016-11-26 17:05:43 -0500

    Creacion de registros con TDD, Clase 8

---
+++ tests/features/CreatePostsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class CreatePostsTest extends FeatureTestCase
+{
+    function test_a_user_create_a_post()
+    {
+        //Having / Teniendo
+        $title = 'Esta es una pregunta';
+        $content = 'Este es el contenido';
+
+        $this->actingAs($user = $this->defaultUser());
+
+        //When / Cuando
+        $this->visit(route('posts.create'))
+            ->type($title, 'title')
+            ->type($content, 'content')
+            ->press('Publicar');
+
+        //Then / Entonces
+        $this->seeInDatabase('posts', [
+            'title'     => $title,
+            'content'   => $content,
+            'pending'   => true,
+            'user_id'   => $user->id,
+        ]);
+
+        // Test a user is redirected to the posts details after creating it
+        $this->see($title);
+    }
+}
