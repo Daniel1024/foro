3cd91762bfddb10a3054e4fde5e09f8cc7860704
Daniel Lopez <tierra_santa_9@hotmail.com>
2016-12-01 12:00:21 -0500

    Listar Registros, Clase 16

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post, $slug) {
         //abort_unless($post->slug == $slug, 404);
 
