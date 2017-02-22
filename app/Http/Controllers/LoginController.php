2f161b3e5d53abd8bfac97890558609f1bc48572
Daniel López <tierra_santa_9@hotmail.com>
2017-02-22 15:16:15 -0500

    Clase 33 lista

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Token;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class LoginController extends Controller
 {
-    public function login(Token $token)
+    public function login(string $token)
     {
-        Auth::login($token->user);
 
-        $token->delete();
+        $token = Token::findActive($token);
+        //dd(Token::query()->orderBy('id', 'DESC')->first()->created_at >= Carbon::parse('30 minutes'));
+
+        if ($token == null) {
+            alert('Este enlace ya expiró, por favor solicita otro', 'danger');
+
+            return redirect()->route('token');
+        }
+
+        $token->login();
 
         return redirect('/');
     }
--- app/Token.php
+++ app/Token.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use App\Mail\TokenMail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\{
+    Auth, Mail
+};
 
 class Token extends Model
 {
@@ -33,8 +36,28 @@ class Token extends Model
         return $token;
     }
 
+    public static function findActive(string $token)
+    {
+        return static::query()
+            ->where('token', $token)
+            ->where('created_at', '>=', Carbon::parse('-30 minutes'))
+            ->first();
+    }
+
     public function sendByEmail()
     {
         Mail::to($this->user)->send(new TokenMail($this));
+    }
+
+    public function login()
+    {
+        Auth::login($this->user);
+
+        $this->delete();
+    }
+
+    public function getUrlAttribute()
+    {
+        return route('login', $this->token);
     }
 }
