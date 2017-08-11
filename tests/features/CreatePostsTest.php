301b2ea8a8dc66520cbc8c20f38f2d3a0946cf41
Daniel López <d.lopez.1740@gmail.com>
2017-08-11 17:47:18 -0500

    Lesson 11

--- tests/features/CreatePostsTest.php
+++ tests/features/CreatePostsTest.php
@@ -36,4 +36,18 @@ class CreatePostsTest extends FeatureTestCase
             ->seePageIs(route('login'));
     }
 
+    function test_create_post_form_validation()
+    {
+        $this->actingAs($this->defaultUser())
+            ->visit(route('posts.create'))
+            ->press('Publicar')
+            ->seePageIs(route('posts.create'))
+            ->seeErrors([
+                'title' => 'El campo título es obligatorio',
+                'content' => 'El campo contenido es obligatorio',
+            ]);
+            /*->seeInElement('#field_title .help-block', 'El campo título es obligatorio')
+            ->seeInElement('#field_content .help-block', 'El campo contenido es obligatorio');*/
+    }
+
 }
