00040e4422ceb691b0702db1cc2352d3ba0ce48f
Daniel López <d.lopez.1740@gmail.com>
2017-08-12 21:54:47 -0500

    Lesson 17

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function index()
+    {
+        $posts = Post::query()
+            ->orderBy('created_at', 'DESC')
+            ->paginate();
+
+        //dd($posts->pluck('created_at')->toArray());
+
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post, $slug)
     {
         if ($post->slug != $slug) {
---
+++ tests/features/PostsListTest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+use App\Post;
+use Carbon\Carbon;
+
+class PostsListTest extends FeatureTestCase
+{
+
+    public function test_a_user_can_see_the_posts_list_and_go_the_details()
+    {
+        $post = $this->createPost([
+            'title' => '¿Debo utilizar Laravel 5.3 o 5.1 LTS?'
+        ]);
+
+        $this->visit('/')
+            ->seeInElement('h1', 'Posts')
+            ->see('¿Debo utilizar Laravel 5.3 o 5.1 LTS?')
+            ->click($post->title)
+            ->seePageIs($post->url);
+    }
+
+    public function test_the_posts_are_paginated()
+    {
+        $first = factory(Post::class)->create([
+            'title' => 'Post más antiguo',
+            'created_at' => Carbon::now()->subDays(2),
+        ]);
+
+        factory(Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDay(),
+        ]);
+
+        $last = factory(Post::class)->create([
+            'title' => 'Post más reciente',
+        ]);
+
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click('2')
+            ->see($first->title)
+            ->dontSee($last->title);
+    }
+}
