a658684329fba810282c0efd71174c8ea77afa0e
Daniel López <d.lopez.1740@gmail.com>
2017-08-15 22:40:26 -0500

    Lesson 36

---
+++ tests/Browser/CreatePostsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Post;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreatePostsTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    function test_a_user_create_a_post()
+    {
+        // Having - Teniendo
+        $title = 'Esto es una pregunta';
+        $content = 'Este es el contenido';
+        $user = $this->defaultUser();
+
+        $this->browse(function (Browser $browser) use ($title, $content, $user){
+
+            $browser->loginAs($user)
+                ->visitRoute('posts.create')
+                ->type('title', $title)
+                ->type('content', $content)
+                ->press('Publicar')
+                ->assertPathIs('/posts/1-esto-es-una-pregunta');
+        });
+
+        // Then - Entonces
+        $this->assertDatabaseHas('posts', [
+            'title' => $title,
+            'content' => $content,
+            'pending' => true,
+            'user_id' => $user->id,
+        ]);
+
+        $post = Post::query()->first();
+
+        $this->assertDatabaseHas('subscriptions', [
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+        ]);
+
+    }
+
+    function test_creating_a_post_requires_authenication()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('posts.create')
+                ->assertRouteIs('token');
+        });
+    }
+
+    function test_create_post_form_validation()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->defaultUser())
+                ->visitRoute('posts.create')
+                ->press('Publicar')
+                ->assertRouteIs('posts.create')
+                ->assertSeeErrors([
+                    'title' => 'El campo título es obligatorio',
+                    'content' => 'El campo contenido es obligatorio',
+                ]);
+        });
+
+    }
+}
